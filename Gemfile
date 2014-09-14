99fe645785044c6b6dbe32c86c59ca42897759ad
pranay <pranay.pramod@gmail.com>
2014-09-13 23:38:12 -0400

    tweaks

--- Gemfile
+++ Gemfile
@@ -17,7 +17,10 @@ gem 'coffee-rails', '~> 4.0.0'
 # gem 'therubyracer',  platforms: :ruby
 
 # Use jquery as the JavaScript library
-gem 'jquery-rails'
+#gem 'jquery-rails'
+#gem 'jquery-ui-rails', '~> 4.2.1'
+#gem 'jquery-rails', '~> 2.3.0'
+gem "jquery-rails", "< 3.0.0"
 # Turbolinks makes following links in your web application faster. Read more: https://github.com/rails/turbolinks
 gem 'turbolinks'
 # Build JSON APIs with ease. Read more: https://github.com/rails/jbuilder
--- app/admin/contract.rb
+++ app/admin/contract.rb
@@ -28,6 +28,34 @@ scope :all, :default => true
     actions
   end
 
+  form do |f|
+    f.inputs "Contract Details" do
+    f.input :account, :label => "ACCOUNT"
+    f.input :customer, :label => "CUSTOMER"
+    f.input :title, :label => "TITLE"
+    f.input :contract_no, :label => "CONTRACT_NO"
+    f.input :status, :label => "STATUS"
+    f.input :start_date, :label => "START_DATE", :as => :string, :input_html => {:class => "datepicker hasDatePicker"}
+    f.input :current_end_date, :label => "CURRENT END DATE" , :as => :string, :input_html => {:class => "datepicker hasDatePicker"}
+    f.input :end_date_w_options, :label => "END DATE W OPTIONS", :as => :string, :input_html => {:class => "datepicker hasDatePicker"}
+    f.input :revenue_to_date, :label => "REVENUE TO DATE"
+    f.input :funded_amount, :label => "FUNDED AMOUNT"
+    f.input :company, :label => "COMPANY"
+    f.input :div_pool, :label => "DIV POOL"
+    f.input :operating_div, :label => "OPERATING DIV"
+    f.input :basis, :label => "BASIS"
+    f.input :vehicle, :label => "VEHICLE"
+    f.input :contract_type, :label => "CONTRACT TYPE"
+    f.input :ecs_role, :label => "ECS ROLE"
+    f.input :signed_contract_amount, :label => "SIGNED CONTRACT AMOUNT"
+    f.input :account_ex, :label => "ACCOUNT EX"
+    f.input :director, :label => "DIRECTOR"
+    f.input :project_manager, :label => "PROJECT MANAGER"
+    f.input :cont_admin, :label => "CONT ADMIN"
+    f.actions :submit, :cancel
+  end
+  end
+
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
