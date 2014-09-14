c0769b0a8ad8bb9507ed923a20d66f8f7e5ba15b
pranay <pranay.pramod@gmail.com>
2014-09-14 00:18:43 -0400

    more validations

--- app/models/contract.rb
+++ app/models/contract.rb
@@ -1,10 +1,19 @@
 class Contract < ActiveRecord::Base
-validates :account, presence: true
-	validates :customer, presence: true
-	validates :title, presence: true
-	validates :contract_no, presence: true, uniqueness: true
+	validates_presence_of :account, :message => "Can't be blank"
+	validates_presence_of :customer, :message => "Can't be blank"
+	validates_presence_of :title, message: "Can't be blank"
+	validates_presence_of :contract_no, message: "Can't be blank"
+	validates_uniqueness_of :contract_no, message: "Contract no already exists"
 	validates :status, inclusion: {in: %w(A C),
 				message: "Value should be A or C"}
+	validate :date_range_checker
+
+private
+def date_range_checker
+	 if current_end_date < start_date || current_end_date > end_date_w_options
+		errors.add(:current_end_date, "Should be between start_date and end_date_w_options")
+	end
+end
 
 before_save	:calculate_funds_remaining
 
